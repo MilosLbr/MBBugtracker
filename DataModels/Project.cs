12d3d6187f1fb8aba1355397e7d6f92dc7bf1fbd
MilosLbr <brankovicmilos@hotmail.com>
2020-07-04 19:10:36 +0200

    Add start date prop to Project model class

--- DataModels/Project.cs
+++ DataModels/Project.cs
@@ -9,6 +9,7 @@ namespace DataModels
         public int Id { get; set; }
         public string ProjectName { get; set; }
         public string Description { get; set; }
+        public DateTime StartDate { get; set; }
 
         public virtual ICollection<Ticket> ProjectTickets { get; set; }
         public virtual ICollection<ProjectsAndUsers> ProjectsAndUsers { get; set; }
---
+++ MbBugtracker/Data/Migrations/20200704170714_AddStartDateToProject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MbBugtracker.Data.Migrations
+{
+    public partial class AddStartDateToProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Projects",
+                nullable: false,
+                defaultValue: new DateTime(2020, 6, 20));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Projects");
+        }
+    }
+}
