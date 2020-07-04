bd7b38682777fe2c0bdced13f72c2d2660a7bbce
MilosLbr <brankovicmilos@hotmail.com>
2020-07-04 19:36:05 +0200

    Add relation between project and application user model

--- DataModels/Project.cs
+++ DataModels/Project.cs
@@ -11,6 +11,9 @@ namespace DataModels
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
 
+        public string ApplicationUserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; } // ProjectOwner
+
         public virtual ICollection<Ticket> ProjectTickets { get; set; }
         public virtual ICollection<ProjectsAndUsers> ProjectsAndUsers { get; set; }
 
